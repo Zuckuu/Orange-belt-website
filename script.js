4c5aa81c13bc149a01d6d2a1b703bba654c383cc
Zuckuu <entbit12@gmail.com>
2022-09-13 22:00:05 -0500

    this

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// make the popup
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
